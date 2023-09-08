bfa473f225fe12801c5aeff4c769aa1c46fcd60b
Benardidoko <benardidoko@yahoo.com>
2023-09-08 07:57:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: if zero(0), successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the coutcome is a piece of art,\n");return (0);
+}
