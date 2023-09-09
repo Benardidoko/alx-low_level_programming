a2beae197444f0cdf8f33fae17057dcc66d8364e
Benardidoko <benardidoko@yahoo.com>
2023-09-09 01:59:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,27 +3,27 @@
 /**
  * main - Entry point
  *
- * Description: Prints all numbers of base 16 in lowercase, followed by a new line.
- *              Uses putchar function only three times.
+ * Description: Prints all numbers of base 16 in lowercase,
+ *              followed by a new line. Uses putchar function only three times.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char hex_digit;
+    char hex_digit;
 
-	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
-	{
-		putchar(hex_digit);
-	}
+    for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
 
-	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
-	{
-		putchar(hex_digit);
-	}
+    for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+    {
+        putchar(hex_digit);
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
 
