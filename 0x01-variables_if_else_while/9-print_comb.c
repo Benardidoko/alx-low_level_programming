26421e153b1d833473ecd66753772d382916cddd
Benardidoko <benardidoko@yahoo.com>
2023-09-09 02:27:49 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,21 @@
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar(num + '0');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
 
-        if (num < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		if (num < 9)
+		{
+			putchar(',');
+			putchar('	'); /* Tab character for indentation */
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
