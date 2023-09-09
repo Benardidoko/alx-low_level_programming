9fedb327d7a90db8dfe39dc350fa84ceef2bfa2a
Benardidoko <benardidoko@yahoo.com>
2023-09-09 00:47:56 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,21 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the alphabet in lowercase, followed by a new line.
+ * Description: This program prints the alphabet in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c; /* Declare a variable to store the character */
+	char c; /* Declare a variable to store the character */
 
-    for (c = 'a'; c <= 'z'; c++) /* Loop through the lowercase alphabet */
-    {
-        putchar(c); /* Use putchar to print each character */
-    }
+	for (c = 'a'; c <= 'z'; c++) /* Loop through the lowercase alphabet */
+	{
+		putchar(c); /* Use putchar to print each character */
+	}
 
-    putchar('\n'); /* Print a newline character */
+	putchar('\n'); /* Print a newline character */
 
-    return (0);
+	return (0);
 }
 
