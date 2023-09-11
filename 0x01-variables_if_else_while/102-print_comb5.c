ac2fa3e2e3335b91d66cfcb94f6c21ab425524e3
Benardidoko <benardidoko@yahoo.com>
2023-09-10 18:07:01 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: Prints all possible combinations of two two-digit numbers.
- *     * Numbers should range from 0 to 99.
- *      * Two numbers are separated by a space, and both are printed with two digits.
- *       * Combinations are separated by a comma, followed by a space.
- *        * Combinations are printed in ascending order.
- *         * 00 01 and 01 00 are considered the same combination of the numbers 0 and 1.
- *          *
- *           * Return: Always 0 (Success)
- *            */
+ * main - Entry point
+ *
+ * Description: Print all possible combinations of two two-digit numbers.
+ * The numbers should be in ascending order and separated by a comma and space.
+ * No trailing comma should appear at the end of the output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int i, j;
+    int tens1, ones1, tens2, ones2;
 
-			for (i = 0; i < 100; i++)
-					{
-								for (j = i; j < 100; j++)
-											{
-															if (i != j)
-																			{
-																								putchar((i / 10) + '0');
-																												putchar((i % 10) + '0');
-																																putchar(' ');
-																																				putchar((j / 10) + '0');
-																																								putchar((j % 10) + '0');
-																																												if (i < 98 || j < 99)
-																																																	{
-																																																							putchar(',');
-																																																												putchar(' ');
-																																																																}
-																																															}
-																	}
-									}
+    for (tens1 = 0; tens1 <= 9; tens1++)
+    {
+        for (ones1 = 0; ones1 <= 9; ones1++)
+        {
+            for (tens2 = tens1; tens2 <= 9; tens2++)
+            {
+                for (ones2 = ones1 + 1; ones2 <= 9; ones2++)
+                {
+                    putchar('0' + tens1);
+                    putchar('0' + ones1);
+                    putchar(' ');
+                    putchar('0' + tens2);
+                    putchar('0' + ones2);
 
-				putchar('\n');
+                    if (!(tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9))
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
 
-					return (0);
+    putchar('\n');
+
+    return (0);
 }
 
