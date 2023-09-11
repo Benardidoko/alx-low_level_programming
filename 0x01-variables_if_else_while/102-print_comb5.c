592b4597274efdcc8f6a75472cf8a7c5328052f0
Benardidoko <benardidoko@yahoo.com>
2023-09-10 18:20:10 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,34 +11,37 @@
  */
 int main(void)
 {
-    int tens1, ones1, tens2, ones2;
+	int tens1, ones1, tens2, ones2;
 
-    for (tens1 = 0; tens1 <= 9; tens1++)
-    {
-        for (ones1 = 0; ones1 <= 9; ones1++)
-        {
-            for (tens2 = tens1; tens2 <= 9; tens2++)
-            {
-                for (ones2 = ones1 + 1; ones2 <= 9; ones2++)
-                {
-                    putchar('0' + tens1);
-                    putchar('0' + ones1);
-                    putchar(' ');
-                    putchar('0' + tens2);
-                    putchar('0' + ones2);
+	for (tens1 = 0; tens1 <= 9; tens1++)
+	{
+		for (ones1 = 0; ones1 <= 9; ones1++)
+		{
+			for (tens2 = 0; tens2 <= 9; tens2++)
+			{
+				for (ones2 = 0; ones2 <= 9; ones2++)
+				{
+					if (tens1 < tens2 || (tens1 == tens2 && ones1 < ones2))
+					{
+						putchar('0' + tens1);
+						putchar('0' + ones1);
+						putchar(' ');
+						putchar('0' + tens2);
+						putchar('0' + ones2);
 
-                    if (!(tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+						if (!(tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
