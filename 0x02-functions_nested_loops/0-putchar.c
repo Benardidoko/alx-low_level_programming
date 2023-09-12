66627bfb3acd01f97876a6fe9f6240d583f37d8d
Benardidoko <benardidoko@yahoo.com>
2023-09-12 12:46:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,25 @@
+/*
+ * File: _putchar.c
+ *
+ * Description: This program prints the string "_putchar" followed by a new line.
+ *
+ * Return: 0 on success.
+ */
+
 #include <stdio.h>
 
-int main() {
-    printf("_putchar\n");
-    return 0;
+int main(void) {
+    /* Print the string "_putchar" followed by a new line. */
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return (0);
 }
 
