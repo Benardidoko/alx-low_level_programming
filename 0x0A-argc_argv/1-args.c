6ac4f2849a146d0a9d163460c6c7560c48b31a2b
Benardidoko <benardidoko@yahoo.com>
2023-10-04 03:57:17 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - The ntry point of the program
+ * @argc: The argument counts
+ * @argv: Argument Lists
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
