bec346a715257ab1afbeeba20e5fa3f2bac7896e
Benardidoko <benardidoko@yahoo.com>
2023-10-04 19:26:52 -0700

    hungry

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * word_counter - function counts the number of words in a string
+ * @s: string representation
+ * Return: returns number of words
+ */
+int word_counter(char *s)
+{
+	int a, c, w;
+
+	a = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			a = 0;
+		else if (a == 0)
+		{
+			a = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+
+/**
+ * strtow - splits strings to words
+ * @str: string representatn
+ * Return: returns pointer to an array of string
+ */
+char **strtow(char *str)
+{
+	char **m, *t;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = word_counter(str);
+	if (words == 0)
+		return (NULL);
+	m = (char **) malloc(sizeof(char *) * (words + 1));
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
