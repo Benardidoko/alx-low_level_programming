26ba2145b0fc0fc6f526926a37579efbdab97068
Benardidoko <benardidoko@yahoo.com>
2023-10-04 19:52:56 -0700

    stressed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,51 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * word_counter - function counts the number of words in string
- * @s: string representation
- * Return: returns number of words
+ * count_word - the function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
  */
-int word_counter(char *s)
+int count_word(char *s)
 {
-	int a, c, w;
-
-	a = 0;
+	int flag, c, w;
+	flag = 0;
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-			a = 0;
-		else if (a == 0)
 		{
-			a = 1;
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			flag = 1;
 			w++;
 		}
-	}
-	return (w);
-}
-
-/**
- * strtow - splits a string to these words
- * @str: string representation
- * Return: returns pointer to an array of string
- */
-char **strtow(char *str)
-{
-	char **m, *t;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-
-	while (*(str + len))
-		len++;
-	words = word_counter(str);
-	if (words == 0)
-		return (NULL);
-	m = (char **) malloc(sizeof(char *) * (words + 1));
-	if (m == NULL)
-		return (NULL);
-	for (i = 0; i <= len; i++)
-	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
