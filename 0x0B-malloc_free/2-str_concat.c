a9793c4474f9822cf54bae6a26a700edfdf9583a
Benardidoko <benardidoko@yahoo.com>
2023-10-04 18:03:55 -0700

    ok

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,49 +3,44 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
+ * str_concat - this concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
  * Return: concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
-    unsigned int m = 0, n = 0;
-    char *ptr, *ret;
+	unsigned int m = 0, n = 0;
+	char *ptr, *ret;
 
-    if (s1 != NULL)
-    {
-        ptr = s1;
-        while (*ptr++)
-            m++;
-    }
-    else
-    {
-        s1 = "";
-    }
-
-    if (s2 != NULL)
-    {
-        ptr = s2;
-        while (*ptr++)
-            n++;
-    }
-    else
-    {
-        s2 = "";
-    }
-
-    ret = malloc(m + n + 1);
-    if (!ret)
-        return (NULL);
-
-    ptr = ret;
-    while (*s1)
-        *ptr++ = *s1++;
-    while (*s2)
-        *ptr++ = *s2++;
-    *ptr = '\0';
-
-    return (ret);
+	ptr = s1;
+	if (s1)
+	{
+		while (*ptr++)
+			m++;
+	}
+	else
+	{
+		s1 = "";
+	}
+	ptr = s2;
+	if (s2)
+	{
+		while (*ptr++)
+			m++;
+	}
+	else
+	{
+		s2 = "";
+	}
+	ret = malloc(m + n + 1);
+	if (!ret)
+		return (NULL);
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
+	return (ret);
 }
-
