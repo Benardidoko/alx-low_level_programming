06e8dce1f5b5cb824551571c6e2cffff1593ea39
Benardidoko <benardidoko@yahoo.com>
2023-10-10 20:04:51 -0700

    ahhhh

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,18 @@
+#include <stdlib.h>
 #include "dog.h"
+
 /**
- *  * free_dog - frees a fucking dogs
- *   * @d: pointer to the struct dog
- *    */
+ *  * free_dog - frees dogs
+ *   * @d: pointer to dog to free
+ *    *
+ *     * Return: void
+ *      */
 void free_dog(dog_t *d)
 {
-		if (!d)
-					return;
-			free(d->name);
-				free(d->owner);
-					free(d);
+		if (d)
+				{
+							free(d->name);
+									free(d->owner);
+											free(d);
+												}
 }
