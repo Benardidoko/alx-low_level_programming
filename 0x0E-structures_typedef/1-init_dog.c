5c4abc17e28510ef37299f0b98fcbe121c5efd10
Benardidoko <benardidoko@yahoo.com>
2023-10-10 19:27:02 -0700

    alx

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ *  * init_dog - variable of type struct dog
+ *   *
+ *    * @d: pointer to the struct dog
+ *     * @name: pointer to the name dog
+ *      * @age: age dog
+ *       * @owner: pointer to the owner dog
+ *        */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		if (!d)	/*return void if d is NULL*/
+					return;
+			/*initializes the elements of the structure, dereference*/
+			d->name = name;
+				d->age = age;
+					d->owner = owner;
+}
