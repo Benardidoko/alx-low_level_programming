8cba95c8305c03f51968885f2f7926cd2229476f
Benardidoko <benardidoko@yahoo.com>
2023-10-10 20:01:23 -0700

    tired

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ *  * free_dog - frees a fucking dogs
+ *   * @d: pointer to the struct dog
+ *    */
+void free_dog(dog_t *d)
+{
+		if (!d)
+					return;
+			free(d->name);
+				free(d->owner);
+					free(d);
+}
