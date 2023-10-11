c236b209ba862d6715fa3dc697dc41a841b38b25
Benardidoko <benardidoko@yahoo.com>
2023-10-10 19:52:53 -0700

    4-main.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <strlen.c>
+#include <strcpy.c>
+/**
+ *  * new_dog - creates a new struct dog
+ *   * @name: dog name
+ *    * @age: dog age
+ *     * @owner: dog owner
+ *      *
+ *       * Return: returnt to pinter a new strcut dog
+ *        */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+		dog_t *dog = NULL;
+
+			dog = malloc(sizeof(dog_t));
+				if (!dog)
+						{
+									free(dog);
+											return (dog);
+												}
+					dog->name = malloc(sizeof(char) * _strlen(name) + 1);
+						if (!dog->name)
+								{
+											free(dog);
+													return (dog);
+														}
+							dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+								if (!dog->owner)
+										{
+													free(dog->name);
+															free(dog);
+																	return (dog);
+																		}
+									_strcpy(dog->name, name);
+										dog->age = age;
+											_strcpy(dog->owner, owner);
+												return (dog);
+}
