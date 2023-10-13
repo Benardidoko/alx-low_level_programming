f87f408831b5140100ca4c7b8c12729636f250ae
Benardidoko <benardidoko@yahoo.com>
2023-10-12 18:54:04 -0700

    okayyy sleepy

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * print_all_types - prints anything, given a list of types of arguments.
- * @type_list: list of types of arguments.
- * @argument_list: variable arguments.
+ * print_all - Prints anything based on the format specifier
+ * @format: A list of types of arguments
  */
-void print_all_types(const char * const type_list, ...)
+void print_all(const char * const format, ...)
 {
-    int i = 0;
-    char *str, *sep = "";
+	int i = 0;
+	char *sep = "";
+	va_list args;
 
-    va_list argument_list;
+	va_start(args, format);
 
-    va_start(argument_list, type_list);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				{
+					char *str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+				}
+				break;
+			default:
+				i++;
+				continue;
+		}
+		sep = ", ";
+		i++;
+	}
 
-    if (type_list)
-    {
-        while (type_list[i])
-        {
-            switch (type_list[i])
-            {
-                case 'c':
-                    printf("%s%c", sep, va_arg(argument_list, int));
-                    break;
-                case 'i':
-                    printf("%s%d", sep, va_arg(argument_list, int));
-                    break;
-                case 'f':
-                    printf("%s%f", sep, va_arg(argument_list, double));
-                    break;
-                case 's':
-                    str = va_arg(argument_list, char *);
-                    if (!str)
-                        str = "(nil)";
-                    printf("%s%s", sep, str);
-                    break;
-                default:
-                    i++;
-                    continue;
-            }
-            sep = ", ";
-            i++;
-        }
-    }
-    printf("\n");
-    va_end(argument_list);
+	printf("\n");
+	va_end(args);
 }
 
